8acfaa233f408e442116539c10814a1edd30a39c
mugx <andrea.mugx@gmail.com>
2016-03-17 00:13:51 +0100

    refactoring class prefix

---
+++ PsyNaut/PsyNaut/Sources/PNItem.h
@@ -0,0 +1,13 @@
+//
+//  PNItem.h
+//  Psynaut
+//
+//  Created by mugx on 16/03/16.
+//  Copyright © 2016 mugx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PNItem : NSObject
+
+@end
---
+++ PsyNaut/PsyNaut/Sources/PNItem.m
@@ -0,0 +1,13 @@
+//
+//  PNItem.m
+//  Psynaut
+//
+//  Created by mugx on 16/03/16.
+//  Copyright © 2016 mugx. All rights reserved.
+//
+
+#import "PNItem.h"
+
+@implementation PNItem
+
+@end
